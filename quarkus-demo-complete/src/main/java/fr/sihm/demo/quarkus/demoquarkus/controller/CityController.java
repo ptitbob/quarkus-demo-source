4e30a9246966ea79f863d1cff0eda044e2d48f6e
ptitbob <github@shipstone.org>
2019-09-26 21:05:26 +0200

    demo :: quarkus :: Ajout de la pagination sur l'API des villes

--- quarkus-demo-complete/src/main/java/fr/sihm/demo/quarkus/demoquarkus/controller/CityController.java
+++ quarkus-demo-complete/src/main/java/fr/sihm/demo/quarkus/demoquarkus/controller/CityController.java
@@ -1,13 +1,17 @@
 package fr.sihm.demo.quarkus.demoquarkus.controller;
 
 import fr.sihm.demo.quarkus.demoquarkus.domain.City;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.panache.common.Sort;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("villes")
@@ -15,8 +19,25 @@ public class CityController {
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public List<City> getAll() {
-    return City.listAll(Sort.by("insee"));
+  public Response getAll(
+      @QueryParam("page") @DefaultValue("0") int page,
+      @QueryParam("size") @DefaultValue("20") int pageSize
+  ) {
+    PanacheQuery<City> cityPanacheQuery = City.findAll(Sort.by("insee")).page(page, pageSize);
+    if (cityPanacheQuery.count() == 0) {
+      return Response.noContent().build();
+    }
+    Response.ResponseBuilder response;
+    if (cityPanacheQuery.pageCount() == 1) {
+      response = Response.ok(cityPanacheQuery.list());
+    } else {
+      response = Response.status(Response.Status.PARTIAL_CONTENT).entity(cityPanacheQuery.list());
+    }
+    response.header("x-total-page", cityPanacheQuery.pageCount());
+    response.header("x-total-element", cityPanacheQuery.count());
+    response.header("x-current-page", cityPanacheQuery.page().index);
+    response.header("x-page-size", cityPanacheQuery.page().size);
+    return response.build();
   }
 
   @GET
