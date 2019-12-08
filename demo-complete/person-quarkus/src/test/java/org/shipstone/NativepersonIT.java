3d8a844f25ac6e387c0a55ccba9f5534e1a52ecb
ptitbob <github@shipstone.org>
2019-12-08 02:00:13 +0100

    Initialisation service personnes

---
+++ demo-complete/person-quarkus/src/test/java/org/shipstone/NativepersonIT.java
@@ -0,0 +1,9 @@
+package org.shipstone;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativepersonIT extends personTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ demo-complete/person-spring/src/test/java/org/shipstone/person/PersonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shipstone.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
